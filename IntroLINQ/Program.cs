446973ff4371c9498fd7043a2a30ae46163a8053
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-17 16:09:39 -0500

    all files listed and sorted by size.

--- IntroLINQ/Program.cs
+++ IntroLINQ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace IntroLINQ
 {
@@ -6,7 +7,31 @@ namespace IntroLINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+
+    public class FileInfoComparer : System.Collections.Generic.IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
