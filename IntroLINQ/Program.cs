15288a945f28cf99796208e3cf6331e105e371be
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-17 15:49:39 -0500

    Add project files.

---
+++ IntroLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
